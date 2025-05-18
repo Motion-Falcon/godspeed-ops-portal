0e91767a0b0498b4217ac2c82c059e61b53238cd
abhishek-motionfalcon <abhishek@motionfalcon.com>
2025-05-18 20:28:09 +0530

    UI and form changes

--- client/src/components/AppHeader.tsx
+++ client/src/components/AppHeader.tsx
@@ -2,7 +2,6 @@ import { ReactNode } from 'react';
 import godspeedLogo from '../assets/logos/godspped-logo-fulllength.png';
 import '../styles/components/header.css';
 import { useNavigate } from 'react-router-dom';
-import { ThemeToggle } from './theme-toggle';
 interface AppHeaderProps {
   title: string;
   actions?: ReactNode;
@@ -24,7 +23,6 @@ export function AppHeader({
           <div className="logo-container" onClick={() => navigate('/')}>
             <img src={godspeedLogo} alt="Godspeed Logo" className="header-logo" />
           </div>
-          <ThemeToggle />
         </div>
         <h1>{title}</h1>
         <div className="header-actions">
